f5b8e36f3d254eb018a4e3daf9d7db9093fa818d
Imtiaz Epu <imtiazepu@programmer.net>
2019-03-13 03:00:47 +0600

    header & footer file added

---
+++ partials/_footer.php
@@ -0,0 +1,14 @@
+<footer class="text-muted">
+    <div class="container">
+        <p class="float-right">
+            <a href="#">Back to top</a>
+        </p>
+        <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+        <p>New to Bootstrap? <a href="https://getbootstrap.com/">Visit the homepage</a> or read our <a
+                href="/docs/4.3/getting-started/introduction/">getting started guide</a>.</p>
+    </div>
+</footer>
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
