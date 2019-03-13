4001576fdd90b74dc42712422ff2a8c5f660a5a5
Imtiaz Epu <imtiazepu@programmer.net>
2019-03-14 03:22:58 +0600

    feather-icon package added & configured

--- partials/_dash_footer.php
+++ partials/_dash_footer.php
@@ -8,7 +8,7 @@
 </footer>
 <script src="node_modules/jquery/dist/jquery.min.js"></script>
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
+<script src="node_modules/feather-icons/dist/feather.min.js"></script>
 <script src="asset/js/dashboard.js"></script>
 </body>
 </html>
